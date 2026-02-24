63d70bc8944aaa1abe205150ce4f0538b32dbdfd
Vinay Kokate <vinaynkokate@gmail.com>
2026-02-24 22:34:45 +0530

    should be able to do this level

--- 230226.cpp
+++ 230226.cpp
@@ -1,25 +1,33 @@
-class Solution {
-public:
-    bool hasAllCodes(string s, int k) {
-        set<string>hset;
-        string temp;
-        int n=s.length();
-        int i=0;
-        if(k>n)return false;
-        for(i=0;i<k;i++){
-            temp.push_back(s[i]);
-        }
-        hset.insert(temp);
-        while(i<n){
-            reverse(temp.begin(),temp.end());
-            temp.pop_back();
-            reverse(temp.begin(),temp.end());
-            temp.push_back(s[i]);
-            hset.insert(temp);
-            i++;
-        }
-        if(hset.size()==pow(2,k))return true;
-        return false;
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (cin >> n) {
+        vector<long long> v(n);
+        for (int i = 0; i < n; ++i) {
+            long long x;
+            cin >> x;
+            v[i] = abs(x);
+        }
+
+        sort(v.begin(), v.end());
+
+        long long ans = 0;
+        for (int i = 0; i < n; ++i) {
+            auto it = upper_bound(v.begin() + i + 1, v.end(), 2 * v[i]);
+            ans += distance(v.begin() + i + 1, it);
+        }
+
+        cout << ans << "\n";
     }
-};
+
+    return 0;
+}
