3e557575860507f0b0d5fa15e2bd7e6094e1a751
Vinay Kokate <vinaynkokate@gmail.com>
2026-02-26 22:05:52 +0530

    straight forward

---
+++ 260226.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int carry=0;
+        int i=s.length()-1;
+        int ans=0;
+        while(i){
+            if(s[i]=='1'){
+                if(carry==1){
+                    ans++;
+                }
+                else{
+                    carry=1;
+                    ans+=2;
+                }
+            }
+            else{
+                if(carry==1){
+                    carry=1;
+                    ans+=2;
+                }
+                else{
+                    ans+=1;
+                }
+            }
+            i--;
+        }
+        if(carry)ans++;
+        return ans;
+    }
+};
